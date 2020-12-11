2338e5d92d0082360d8c070cb97a10789adbf4b3
Scoldingcoder <caleb_beard@hotmail.com>
2020-12-11 19:45:54 -0400

    added buttons, only reset works as intended, partial work done on the grid number button

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let root = document.documentElement;
 
+
+
 rowNum = getComputedStyle(root).getPropertyValue("--rows");
 columnNum = getComputedStyle(root).getPropertyValue("--columns");
 
@@ -9,33 +11,90 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
+// Function to creates the initial grid  
+function createDivs(divCount) {
+  for(i = divCount-1; i>=0; i--){
+
+    div = document.createElement('div');
+    div.setAttribute ('class', 'divs');
+    
+    div.style.borderColor = "rgb(238, 232, 232)";
+   
+    div.style.backgroundColor ="white";
+    container.appendChild(div);
+ 
+   }
+
+}
+
+//Selecting the Conatiner div
 container = document.querySelector('.container');
 
-  for(i = count-1; i>=0; i--){
+//Creating divs
+createDivs(count) ;
 
-   div = document.createElement('div');
-   div.setAttribute ('class', 'divs');
-   //div.style.borderStyle = "solid";
-   div.style.borderColor = "rgb(238, 232, 232)";
-   //div.style.boxSizing = "box-border";
-  // div.style.borderWidth = "0.25px";
-   div.style.backgroundColor ="white";
-   container.appendChild(div);
+let divAct = document.getElementsByClassName("divs"); // div to take action on
 
+
+const buttons = document.querySelectorAll('button');
+
+//Listening for the button Clicks
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+  Action=button.id;
+
+  if (Action =="reset"){
+
+    for (var i = 0 ; i < divAct.length; i++) {
+    
+      divAct[i].style.backgroundColor = "white";
+      //divAct[i].style.borderColor = "rgb(238, 232, 232)";
+      
+      }    
+
+  } 
+
+  else if (Action =="gridNumber"){
+
+    for (var i = 0 ; i < divAct.length; i++) {
+    
+      divAct[i].style.backgroundColor = "white";
+      //divAct[i].style.borderColor = "rgb(238, 232, 232)";
+      
+      } 
+
+    
+    number = window.prompt("How many grids per row/column? Please enter a whole number between 1 and 100")
+
+    if ((number > 0) && (number < 65) && (number%1 ==0) ){
+
+    document.querySelectorAll('.divs').forEach(function(a){
+      a.remove()
+      }) 
+      createDivs(number**2);
+    }
+
+    else{
+      window.alert("Whoops,Looks like you made an error")
+    }
   }
 
+  });
 
-  let paint = document.getElementsByClassName("divs");
+});
 
+// Listens for the hover on the the grid
   for (var i = 0 ; i < count; i++) {
       
-    paint[i].addEventListener("mouseenter", function( event ){
+    divAct[i].addEventListener("mouseenter", function( event ){
          
         let r = getRandomInt(255)
         let g = getRandomInt(255)
         let b = getRandomInt(255)
         /*event.target.style.backgroundColor = "black";*/
-        event.target.style.borderColor = `rgb(${r},${g},${b})`;
+        //event.target.style.borderColor = `rgb(${r},${g},${b})`;
         event.target.style.backgroundColor =`rgb(${r},${g},${b})`;
     
       },false);
@@ -43,3 +102,8 @@ container = document.querySelector('.container');
  }
 
   
+
+
+
+
+
